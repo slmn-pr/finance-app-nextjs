12cee81e582c2fed08d2aee6a7e2c871d5541075
Sython <soleimanpoursalman@gmail.com>
2024-08-27 11:06:32 -0700

    add simple page loading ui

--- app/dashboard/settings/layout.js
+++ app/dashboard/settings/layout.js
@@ -1,11 +1,15 @@
+import { Suspense } from "react";
 import SideNav from "./components/side-nav";
+import Loading from "./loading";
 
 const Layout = ({ children }) => {
   return (
     <div className="grid grid-cols-4 gap-8">
       <SideNav />
 
-      <div className="col-span-3">{children}</div>
+      <div className="col-span-3">
+        <Suspense fallback={<Loading />}>{children}</Suspense>
+      </div>
     </div>
   );
 };
